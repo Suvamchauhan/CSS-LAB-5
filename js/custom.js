80add5fcd21ec5e71be7a5b1c3bf0335bae2b11a
Suvamchauhan <113388990+Suvamchauhan@users.noreply.github.com>
2023-10-10 22:15:51 -0400

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+const carouselCars = document.getElementById("carousel-cars");
+
+
+const carousel = new bootstrap.Carousel(carouselCars, {
+    interval: 3000,
+    pause: false,
+    touch: true,
+    wrap: true,
+    keyboard: false
+})
+
+// STEP 16: Change the interval to 5 seconds (set in milliseconds)
+// STEP 17: Try an event handler to zoom up the .carousel-caption after the slide completes sliding - by adding a class when the slide has finished animating, and removing it when it begins
+carouselCars.addEventListener('slid.bs.carousel', event => {
+    // add the .zoomit class for fun
+    carouselCars.classList.add("zoomit")
+})
+
+carouselCars.addEventListener('slide.bs.carousel', event => {
+    // remove the .zoomit class to set up for the next zoomit
+    carouselCars.classList.remove("zoomit")
+})
